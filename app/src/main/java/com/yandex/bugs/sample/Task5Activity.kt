643b43e797226d12227cec2cf66efe471c0e45b3
the-leo <leo.ralnikov@gmail.com>
2025-03-21 17:06:24 +0300

    Task 5: Nicely done

--- app/src/main/java/com/yandex/bugs/sample/Task5Activity.kt
+++ app/src/main/java/com/yandex/bugs/sample/Task5Activity.kt
@@ -6,15 +6,36 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.yandex.bugs.sample.ui.theme.BugsSampleTheme
+import com.yandex.div.DivDataTag
+import com.yandex.div.compose.interop.DivView
+import com.yandex.div.compose.interop.scoped
+import com.yandex.div.core.Div2Context
+import com.yandex.div.core.DivConfiguration
+import com.yandex.div.data.DivParsingEnvironment
+import com.yandex.div.json.ParsingErrorLogger
+import com.yandex.div.markdown.DivMarkdownExtensionHandler
+import com.yandex.div.picasso.PicassoDivImageLoader
+import com.yandex.div2.DivData
+import org.json.JSONObject
 
 class Task5Activity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val divString = assets.open("pong.json").bufferedReader().use { it.readText() }
+        val divJson = JSONObject(divString)
+
+        val templatesJson = divJson.optJSONObject("templates")
+        val cardJson = divJson.getJSONObject("card")
+
+        val environment = DivParsingEnvironment(ParsingErrorLogger.ASSERT).apply {
+            if (templatesJson != null) parseTemplates(templatesJson)
+        }
+
+        val divData = DivData(environment, cardJson)
 
         setContent {
             BugsSampleTheme {
@@ -22,16 +43,25 @@ class Task5Activity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TodoScreen()
+                    Div2Context(
+                        baseContext = this,
+                        configuration = createDivConfiguration(),
+                        lifecycleOwner = this,
+                    ).scoped {
+                        DivView(
+                            data = divData,
+                            tag = DivDataTag("not_so_simple_screen"),
+                        )
+                    }
                 }
             }
         }
     }
 
-    @Composable
-    fun TodoScreen() {
-        Text(
-            text = "TODO"
-        )
+    private fun createDivConfiguration(): DivConfiguration {
+        return DivConfiguration.Builder(PicassoDivImageLoader(this))
+            .extension(DivMarkdownExtensionHandler(this))
+            .visualErrorsEnabled(false)
+            .build()
     }
 }
